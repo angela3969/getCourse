5753749b2285a32095cf71a4eb476eb54df70186
angela3969 <64839887+angela3969@users.noreply.github.com>
2022-03-13 15:53:41 +0800

    Delete getCourse directory

--- getCourse/run.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from app import app
-
-@app.route("/")
-def index():
-    return "Flask Api Start"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=3000,debug=False)
