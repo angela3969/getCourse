2f588d1acec0bcef38a61828c0f1d6e89237aab7
angela3969 <64839887+angela3969@users.noreply.github.com>
2022-03-14 19:20:09 +0800

    Update courseTest.py

--- courseTest.py
+++ courseTest.py
@@ -6,6 +6,7 @@ Created on Sun Mar 13 14:31:41 2022
 @author: angela
 """
 
+import os
 import time
 from selenium import webdriver
 from bs4 import BeautifulSoup
@@ -14,7 +15,13 @@ from selenium.webdriver.common.keys import Keys
 
 
 def getCourse(keyword):
-    driver = webdriver.Chrome('./chromedriver')  # Optional argument, if not specified will search path.
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless") #無頭模式
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options
+    #driver = webdriver.Chrome('./chromedriver')  # Optional argument, if not specified will search path.
 
     driver.get('https://querycourse.ntust.edu.tw/querycourse/#/');
 
