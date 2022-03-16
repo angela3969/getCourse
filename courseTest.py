b17a1e47d40f1d1a90b39482ab7c5633651a6748
angela3969 <64839887+angela3969@users.noreply.github.com>
2022-03-16 12:33:35 +0800

    Update courseTest.py

--- courseTest.py
+++ courseTest.py
@@ -6,9 +6,7 @@ Created on Sun Mar 13 14:31:41 2022
 @author: angela
 """
 
-import os
 import time
-import lxml
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.action_chains import ActionChains
@@ -16,56 +14,55 @@ from selenium.webdriver.common.keys import Keys
 
 
 def getCourse(keyword):
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless") #無頭模式
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-    #driver = webdriver.Chrome('./chromedriver')  # Optional argument, if not specified will search path.
+    driver = webdriver.Chrome('./chromedriver')  # Optional argument, if not specified will search path.
 
-    driver.get('https://querycourse.ntust.edu.tw/querycourse/#/')
-    driver.maximize_window()   
-    driver.set_window_size(1000, 900)  
+    driver.get('https://querycourse.ntust.edu.tw/querycourse/#/');
 
     #time.sleep(5) # Let the user actually see something!
     ActionChains(driver).move_by_offset(500, 400).click().perform() 
     time.sleep(2)
-    search_class = driver.find_element_by_xpath('//input[@aria-label="課程名稱"]') #找到輸入帳號的位置 並先記錄之後要input
+    search_class = driver.find_element_by_xpath('//input[@aria-label="課程名稱"]')
+    #//*[@id="app"]/div[12]/main/div/div/div/div/div[1]/div/div[2]/div/div[1]/div/div[2]/div/div/div[1]/div/input#找到輸入帳號的位置 並先記錄之後要input
     search_class.send_keys(keyword) 
     search_class.send_keys(Keys.ENTER)
 
-    time.sleep(5)
+    time.sleep(2)
 
     soup = BeautifulSoup(driver.page_source, "lxml")
         
     table = soup.find("table",class_="v-datatable v-table theme--light")
     #print(table)
     elements = table.find_all("tr")
-    #print(elements)
+    
     i=0
     courses = {}
     for element in elements:
         data_name = element.find_all("td")
+        print(data_name)
         if(i>1):
-            if(data_name!='<td class="text-xs-center" colspan="12">無資料</td>'):
-                #print(getCourseInfo(data_name))
+            print(getCourseInfo(data_name))
+            if(getCourseInfo(data_name) == '無資料'):
+                courses['無資料'] = '無資料'
+                break
+            else:
                 courses['course'+str(i)]=str(getCourseInfo(data_name))
-            i+=1
-        else:
-            courses['無資料']='無資料'
-    print(courses)
+                
+        i+=1 
     return(courses)        
         
 def getCourseInfo(data):
     courseId = data[0]
-    courseName = data[2]
-    professor = data[6]
-    notice = data[10]
-    courseInfo = [courseId,courseName,professor,notice]
-    #courseInfo[0] = (courseId.split('<td>'))[1]
-    return courseInfo
+    print(data[0])
+    if(str(courseId) == '<td class="text-xs-center" colspan="12">無資料</td>'):
+        return '無資料'
+    else:
+        courseName = data[2]
+        professor = data[6]
+        notice = data[10]
+        courseInfo = [courseId,courseName,professor,notice]
+        #courseInfo[0] = (courseId.split('<td>'))[1]
+        return courseInfo
     
     
     
-#getCourse('python')
+#print(getCourse('python'))
