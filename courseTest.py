d3328e35c2846d5677f89c42f57b506e4f4e86c7
angela3969 <64839887+angela3969@users.noreply.github.com>
2022-03-16 12:47:17 +0800

    Update courseTest.py

--- courseTest.py
+++ courseTest.py
@@ -5,8 +5,11 @@ Created on Sun Mar 13 14:31:41 2022
 
 @author: angela
 """
-
+ def getCourse(keyword):
+     
+import os
 import time
+import lxml
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.action_chains import ActionChains
@@ -14,18 +17,20 @@ from selenium.webdriver.common.keys import Keys
 
 
 def getCourse(keyword):
-    driver = webdriver.Chrome('./chromedriver')  # Optional argument, if not specified will search path.
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless") #無頭模式
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    #driver = webdriver.Chrome('./chromedriver')  # Optional argument, if not specified will search path.
 
-    driver.get('https://querycourse.ntust.edu.tw/querycourse/#/');
+    driver.get('https://querycourse.ntust.edu.tw/querycourse/#/')
+    driver.maximize_window()   
+    driver.set_window_size(1000, 900)  
 
     #time.sleep(5) # Let the user actually see something!
     ActionChains(driver).move_by_offset(500, 400).click().perform() 
-    time.sleep(2)
-    search_class = driver.find_element_by_xpath('//input[@aria-label="課程名稱"]')
-    #//*[@id="app"]/div[12]/main/div/div/div/div/div[1]/div/div[2]/div/div[1]/div/div[2]/div/div/div[1]/div/input#找到輸入帳號的位置 並先記錄之後要input
-    search_class.send_keys(keyword) 
-    search_class.send_keys(Keys.ENTER)
-
     time.sleep(2)
 
     soup = BeautifulSoup(driver.page_source, "lxml")
